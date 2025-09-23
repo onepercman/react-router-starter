a741875066b63d714b5f18d0ee22d694a740a580
onepercman <onepercman@gmail.com>
2025-09-23 22:19:39 +0700

    🚀 update fonts

--- app/root.tsx
+++ app/root.tsx
@@ -23,7 +23,11 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Geist+Mono:wght@100..900&display=swap",
   },
 ]
 
